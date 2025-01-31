fe69e45062b5708f711640dc2532f93162212106
wajidjaved25 <wajidjaved2512@gmail.com>
2025-01-31 15:29:14 +0500

    v 4.1.0

--- routes/web.php
+++ routes/web.php
@@ -94,7 +94,7 @@ Route::middleware(['auth'])->group(function () {
     // Document Viewing Route
 Route::get('/documents/view/{filename}', [DocumentController::class, 'viewDocument'])->name('documents.view');
 Route::get('/documents/view/{filename}', function ($filename) {
-    $path = storage_path("app/documents/$filename");
+    $path = storage_path("app/public/documents/$filename");
 
     // ✅ Ensure file exists
     if (!file_exists($path)) {
